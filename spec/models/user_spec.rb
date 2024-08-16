f922df9f861440d43325671a2f2319cb7cc5d955
shio-san3 <shiofuji47@gmail.com>
2024-08-16 18:27:15 +0900

    単体テストコードの記述の手直し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    it 'nicknameが空だと登録できない' do
-    user = User.new(
-      name: '',
-      email: 'test@example',
-      password: '000000',
-      password_confirmation: '000000',
-      last_name: '佐藤',
-      first_name: '太郎',
-      last_name_kana: 'サトウ',
-      first_name_kana:'タロウ',
-      birth_date: Date.new(1995, 5, 5)
-    )
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
+    it 'nameが空だと登録できない' do
+      user = User.new(
+        name: '',  # nameが空の場合
+        email: 'test@example.com',  # emailアドレス形式を修正
+        password: '000000',
+        password_confirmation: '000000',
+        last_name: '佐藤',
+        first_name: '太郎',
+        last_name_kana: 'サトウ',
+        first_name_kana:'タロウ',
+        birth_date: Date.new(1995, 5, 5)
+      )
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")  # nameに対応するエラーメッセージを確認
     end
+
     it 'emailが空では登録できない' do
-    user = User.new(
-      name: 'furima太郎',
-      email: '',
-      password: '000000',
-      password_confirmation: '000000',
-      last_name: '佐藤',
-      first_name: '太郎',
-      last_name_kana: 'サトウ',
-      first_name_kana:'タロウ',
-      birth_date: Date.new(1995, 5, 5)
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+      user = User.new(
+        name: 'furima太郎',  # nameを使用
+        email: '',  # emailを空に
+        password: '000000',
+        password_confirmation: '000000',
+        last_name: '佐藤',
+        first_name: '太郎',
+        last_name_kana: 'サトウ',
+        first_name_kana:'タロウ',
+        birth_date: Date.new(1995, 5, 5)
+      )
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")  # emailに対応するエラーメッセージを確認
     end
   end
 end
