4d1f2397b8a9cbaa2929766d81aa7dba771905e7
shio-san3 <shiofuji47@gmail.com>
2024-08-20 17:35:34 +0900

    単体テストコード修正⑥

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   # ニックネームのバリデーション
-  validates :name, presence: true, length: { maximum: 6 }
+  validates :name, presence: true
   # パスワードのバリデーション
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "must include both letters and numbers" }
   # お名前(全角)のバリデーション
